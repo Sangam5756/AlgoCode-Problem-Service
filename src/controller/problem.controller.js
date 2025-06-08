1239de226b2373e61bdb5f1b0a267cbbaa61161a
Sangam5756 <sangammunde3@gmail.com>
2025-06-08 14:22:54 +0530

    Fix: migrated from numeric statuscode to http-status-codes library

--- src/controller/problem.controller.js
+++ src/controller/problem.controller.js
@@ -1,33 +1,35 @@
+const { StatusCodes } = require("http-status-codes");
+
 function ping(req, res) {
-  res.status(200).json({ message: "PONG CONTROLLER IS WORKING FINE  " });
+  res.status(StatusCodes.OK).json({ message: "PONG CONTROLLER IS WORKING FINE  " });
 }
 
 function AddProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: " not implemented yet.",
   });
 }
 
 function getProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: " not implemented yet.",
   });
 }
 
 function getProblems(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: " not implemented yet.",
   });
 }
 
 function deleteProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: " not implemented yet.",
   });
 }
 
 function updateProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: " not implemented yet.",
   });
 }
